010fc0320a88d6aaa112678f0a9a2166a719c76a
Enchan <enchant0130@gmail.com>
2023-08-08 18:17:49 +0900

    [Update #9] add libparseropt as dependency for idea cli

---
+++ cli/idea.cpp
@@ -0,0 +1,13 @@
+//
+// IDEA暗号CLI (暗号化/復号)
+//
+#include <iostream>
+
+#include "ideaencrypt/cipher.hpp"
+
+using namespace ideaencrypt;
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
---
+++ cli/keygen.cpp
@@ -0,0 +1,13 @@
+//
+// IDEA暗号CLI (鍵生成)
+//
+#include <iostream>
+
+#include "ideaencrypt/cipher.hpp"
+
+using namespace ideaencrypt;
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
